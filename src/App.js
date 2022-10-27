d566ef3069d5804d051acdd2e5d8709fa143f288
EricChukoltyansky <kazohelmet@gmail.com>
2022-10-27 09:38:56 +0300

    board

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { useState } from "react";
+import styled from "styled-components";
+
+const Board = styled.div`
+  width: 500px;
+  height: 500px;
+  background-color: aquamarine;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+`;
 
 function App() {
-  return <div className="App"></div>;
+  const [board, setBoard] = useState(Array(9).fill(""));
+  return (
+    <div className="App">
+      <div className="board"></div>
+    </div>
+  );
 }
 
 export default App;
